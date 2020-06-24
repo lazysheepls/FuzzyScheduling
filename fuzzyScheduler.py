dc4e7fdbd6f03642da6f3eca82a8d4f08746e26d
z3451444 <z3451444@cse.unsw.edu.au>
2020-06-25 09:17:56 +1000

    WIP: Created the Time class

--- fuzzyScheduler.py
+++ fuzzyScheduler.py
@@ -1,5 +1,6 @@
 from sys import argv
 from re import *
+from cspProblem import Constraint
 
 # read input file
 file_name = argv[1]
@@ -11,12 +12,12 @@ hours_of_day = {"9am":0, "10am":1, "11am":2, "12pm":3, "1pm":4, "2pm":5, "3pm":6
 
 # process inputs
 # for line in lines:
-    ## Constrains
-    # Unary constrains
-    # Binary constrains
-    # Hard domain constrains
+    ## constraints
+    # Unary constraints
+    # Binary constraints
+    # Hard domain constraints
 
-    # Soft deadline constrains
+    # Soft deadline constraints
     # Tasks : task, ⟨name⟩ ⟨duration⟩ - tuple
     # tasks = list()
     # task_regex = search("task, (?P<name>\S*) (?P<duration>\S*)",line)
@@ -26,8 +27,9 @@ hours_of_day = {"9am":0, "10am":1, "11am":2, "12pm":3, "1pm":4, "2pm":5, "3pm":6
 def main():
     lines = read_file_to_lines(file_name)
     domain = get_domain_from_lines(lines)
-    print(lines)
-    print(domain)
+    get_hard_constraints_from_lines(lines)
+    # print(lines)
+    # print(domain)
     # for d in domain:
     #     print(d)
 
@@ -51,14 +53,50 @@ def get_domain_from_lines(lines):
             # get domain for each variable
             for day in days_in_week.values():
                 for hour in hours_of_day.values():
-                    start_time = day + hour
-                    finish_time = start_time + duration
+                    # TODO: create a class to save and compare time
+                    start_time = day + hour #TODO: replce by the time class
+                    finish_time = start_time + duration #TODO: replace by the time class
+                    start_time = Time(day,hour)
 
                     # Remove the time if the task could not finish in one day
                     if finish_time > day + list(hours_of_day.values())[-1]:
                         continue
                     domain[variable_name].append((start_time,finish_time))
     return domain
+
+def get_hard_constraints_from_lines(lines):
+    get_binary_constraints_from_lines(lines)
+
+def get_binary_constraints_from_lines(lines):
+    for line in lines:
+        match = search(r"^constraint, (?P<t1>\S*) (?P<type>\S*) (?P<t2>\S*)",line)
+        if match is not None:
+            t1 = match.group("t1")
+            binary_constraint_type = match.group("type")
+            t2 = match.group("t2")
+
+            if binary_constraint_type == "before":
+                pass
+            elif binary_constraint_type == "after":
+                pass
+            elif binary_constraint_type == "same-day":
+                pass
+            elif binary_constraint_type == "starts-at":
+                pass
+            else :
+                exit("Invalid binary constraint")
+
+            print(t1,binary_constraint_type,t2)
+
+def get_domain_constraints_from_lines(lines):
+    pass
+
+###### Custom Class : Time ######
+class Time:
+    def __init__(self, day, hour):
+        self.time = (day, hour)
+        #TODO: finishe the time add, and time compare
+
 
 if __name__ == '__main__':
     main()         
